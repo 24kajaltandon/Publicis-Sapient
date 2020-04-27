ea5202a283a240f471d7e0efe10e196344dff176
24kajaltandon <24kajaltandon@gmail.com>
2020-04-28 00:17:35 +0530

    week3 assignment added

---
+++ week3_assignment/src/com/sapient/shapes/ShapeRunner.java
@@ -0,0 +1,14 @@
+package com.sapient.shapes;
+
+
+           public class ShapeRunner {
+
+        		public static void main(String[] args) {
+
+        			Shapes shape = new Shapes();
+        			shape.calculateShapeArea(3, 12);// noOfSides, sideLength
+
+        		}
+
+           }
+      
